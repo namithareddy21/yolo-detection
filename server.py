640853b8f0bf1988d251d75d96257f50b7b5acb8
namithareddy21 <245124737109@mvsrec.edu.in>
2025-12-11 20:45:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,16 @@ import numpy as np
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from ultralytics import YOLO
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI()
 
